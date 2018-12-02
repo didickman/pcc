0d17e3929f7f66ca02461d813e2f0825da8750fa
Anders Magnusson <ragge@ludd.ltu.se>
2018-12-02 10:37:21 +0000

    Add floating point definitions for arm.

--- arch/arm/macdefs.h
+++ arch/arm/macdefs.h
@@ -267,3 +267,10 @@ NODE *arm_builtin_va_copy(const struct bitable *bt, NODE *a);
 
 #define COM     "\t@ "
 #define NARGREGS	4
+
+/* floating point definitions */
+#define USE_IEEEFP_32
+#define FLT_PREFIX	IEEEFP_32
+#define USE_IEEEFP_64
+#define DBL_PREFIX	IEEEFP_64
+#define LDBL_PREFIX	IEEEFP_64
