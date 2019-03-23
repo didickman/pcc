0bf7d086a634dc9f73a258fc9887f1722a0088c5
Anders Magnusson <ragge@ludd.ltu.se>
2019-03-23 10:27:23 +0000

    Add missing functions for x80/64.  Remove redundant code.

--- common/softfloat.c
+++ common/softfloat.c
@@ -114,412 +114,6 @@ static int mknormal(FPI *, int *e, MINT *m);
  *	- short is at least 16 bits.
  */
 
-#if 0
-
-/*
- * Supports F- and D-float, used in DEC machines.
- *
- * XXX - assumes that:
- *	- long long is (at least) 64 bits
- *	- int is at least 32 bits.
- *	- short is 16 bits.
- */
-
-/*
- * Useful macros to manipulate the float.
- */
-#define DSIGN(w)	(((w).fd1 >> 15) & 1)
-#define DSIGNSET(w,s)	((w).fd1 = (s << 15) | ((w).fd1 & 077777))
-#define DEXP(w)		(((w).fd1 >> 7) & 0377)
-#define DEXPSET(w,e)	((w).fd1 = (((e) & 0377) << 7) | ((w).fd1 & 0100177))
-#define DMANTH(w)	((w).fd1 & 0177)
-#define DMANTHSET(w,m)	((w).fd1 = ((m) & 0177) | ((w).fd1 & 0177600))
-
-typedef unsigned int lword;
-typedef unsigned long long dword;
-
-#define MAXMANT 0x100000000000000LL
-
-/*
- * Returns a zero dfloat.
- */
-static SF
-nulldf(void)
-{
-	SF rv;
-
-	rv.fd1 = rv.fd2 = rv.fd3 = rv.fd4 = 0;
-	return rv;
-}
-
-/*
- * Convert a (u)longlong to dfloat.
- * XXX - fails on too large (> 55 bits) numbers.
- */
-SF
-soft_cast(CONSZ ll, TWORD t)
-{
-	int i;
-	SF rv;
-
-	rv = nulldf();
-	if (ll == 0)
-		return rv;  /* fp is zero */
-	if (ll < 0)
-		DSIGNSET(rv,1), ll = -ll;
-	for (i = 0; ll > 0; i++, ll <<= 1)
-		;
-	DEXPSET(rv, 192-i);
-	DMANTHSET(rv, ll >> 56);
-	rv.fd2 = ll >> 40;
-	rv.fd3 = ll >> 24;
-	rv.fd4 = ll >> 8;
-	return rv;
-}
-
-/*
- * multiply two dfloat. Use chop, not round.
- */
-SF
-soft_mul(SF p1, SF p2)
-{
-	SF rv;
-	lword a1[2], a2[2], res[4];
-	dword sum;
-
-	res[0] = res[1] = res[2] = res[3] = 0;
-
-	/* move mantissa into lwords */
-	a1[0] = p1.fd4 | (p1.fd3 << 16);
-	a1[1] = p1.fd2 | DMANTH(p1) << 16 | 0x800000;
-
-	a2[0] = p2.fd4 | (p2.fd3 << 16);
-	a2[1] = p2.fd2 | DMANTH(p2) << 16 | 0x800000;
-
-#define MULONE(x,y,r) sum += (dword)a1[x] * (dword)a2[y]; sum += res[r]; \
-	res[r] = sum; sum >>= 32;
-
-	sum = 0;
-	MULONE(0, 0, 0);
-	MULONE(1, 0, 1);
-	res[2] = sum;
-	sum = 0;
-	MULONE(0, 1, 1);
-	MULONE(1, 1, 2);
-	res[3] = sum;
-
-	rv.fd1 = 0;
-	DSIGNSET(rv, DSIGN(p1) ^ DSIGN(p2));
-	DEXPSET(rv, DEXP(p1) + DEXP(p2) - 128);
-	if (res[3] & 0x8000) {
-		res[3] = (res[3] << 8) | (res[2] >> 24);
-		res[2] = (res[2] << 8) | (res[1] >> 24);
-	} else {
-		DEXPSET(rv, DEXP(rv) - 1);
-		res[3] = (res[3] << 9) | (res[2] >> 23);
-		res[2] = (res[2] << 9) | (res[1] >> 23);
-	}
-	DMANTHSET(rv, res[3] >> 16);
-	rv.fd2 = res[3];
-	rv.fd3 = res[2] >> 16;
-	rv.fd4 = res[2];
-	return rv;
-}
-
-SF
-soft_div(SF t, SF n)
-{
-	SF rv;
-	dword T, N, K;
-	int c;
-
-#define SHL(x,b) ((dword)(x) << b)
-	T = SHL(1,55) | SHL(DMANTH(t), 48) |
-	    SHL(t.fd2, 32) | SHL(t.fd3, 16) | t.fd4;
-	N = SHL(1,55) | SHL(DMANTH(n), 48) |
-	    SHL(n.fd2, 32) | SHL(n.fd3, 16) | n.fd4;
-
-	c = T > N;
-	for (K = 0; (K & 0x80000000000000ULL) == 0; ) {
-		if (T >= N) {
-			T -= N;
-			K |= 1;
-		}
-		T <<= 1;
-		K <<= 1;
-	}
-	rv.fd1 = 0;
-	DSIGNSET(rv, DSIGN(t) ^ DSIGN(n));
-	DEXPSET(rv, DEXP(t) - DEXP(n) + 128 + c);
-	DMANTHSET(rv, K >> 48);
-	rv.fd2 = K >> 32;
-	rv.fd3 = K >> 16;
-	rv.fd4 = K;
-	return rv;
-}
-
-/*
- * Negate a float number. Easy.
- */
-SF
-soft_neg(SF sf)
-{
-	int sign = DSIGN(sf) == 0;
-	DSIGNSET(sf, sign);
-	return sf;
-}
-
-/*
- * Return true if fp number is zero.
- */
-int
-soft_isz(SF sf)
-{
-	return (DEXP(sf) == 0);
-}
-
-int
-soft_cmp_eq(SF x1, SF x2)
-{
-	cerror("soft_cmp_eq");
-	return 0;
-}
-
-int
-soft_cmp_ne(SF x1, SF x2)
-{
-	cerror("soft_cmp_ne");
-	return 0;
-}
-
-int
-soft_cmp_le(SF x1, SF x2)
-{
-	cerror("soft_cmp_le");
-	return 0;
-}
-
-int
-soft_cmp_lt(SF x1, SF x2)
-{
-	cerror("soft_cmp_lt");
-	return 0;
-}
-
-int
-soft_cmp_ge(SF x1, SF x2)
-{
-	cerror("soft_cmp_ge");
-	return 0;
-}
-
-int
-soft_cmp_gt(SF x1, SF x2)
-{
-	cerror("soft_cmp_gt");
-	return 0;
-}
-
-/*
- * Convert a fp number to a CONSZ.
- */
-CONSZ
-soft_val(SF sf)
-{
-	CONSZ mant;
-	int exp = DEXP(sf) - 128;
-
-	mant = SHL(1,55) | SHL(DMANTH(sf), 48) |
-            SHL(sf.fd2, 32) | SHL(sf.fd3, 16) | sf.fd4;
-
-	while (exp < 0)
-		mant >>= 1, exp++;
-	while (exp > 0)
-		mant <<= 1, exp--;
-	return mant;
-}
-
-SF
-soft_plus(SF x1, SF x2)
-{
-	cerror("soft_plus");
-	return x1;
-}
-
-SF
-soft_minus(SF x1, SF x2)
-{
-	cerror("soft_minus");
-	return x1;
-}
-
-/*
- * Convert a hex constant to floating point number.
- */
-NODE *
-fhexcon(char *s)
-{
-	cerror("fhexcon");
-	return NULL;
-}
-
-/*
- * Convert a floating-point constant to D-float and store it in a NODE.
- */
-NODE *
-floatcon(char *s)
-{
-	NODE *p;
-	dword mant;
-	SF fl, flexp, exp5;
-	int exp, negexp, bexp;
-
-	exp = 0;
-	mant = 0;
-#define ADDTO(sum, val) sum = sum * 10 + val - '0'
-	for (; *s >= '0' && *s <= '9'; s++) {
-		if (mant<MAXMANT)
-			ADDTO(mant, *s);
-		else
-			exp++;
-	}
-	if (*s == '.') {
-		for (s++; *s >= '0' && *s <= '9'; s++) {
-			if (mant<MAXMANT) {
-				ADDTO(mant, *s);
-				exp--;
-			}
-		}
-	}
-
-	if ((*s == 'E') || (*s == 'e')) {
-		int eexp = 0, sign = 0;
-		s++;
-		if (*s == '+')
-			s++;
-		else if (*s=='-')
-			sign = 1, s++;
-
-		for (; *s >= '0' && *s <= '9'; s++)
-			ADDTO(eexp, *s);
-		if (sign)
-			eexp = -eexp;
-		exp = exp + eexp;
-	}
-
-	negexp = 1;
-	if (exp<0) {
-		negexp = -1;
-		exp = -exp;
-	}
-
-
-	flexp = soft_cast(1, INT);
-	exp5 = soft_cast(5, INT);
-	bexp = exp;
-	fl = soft_cast(mant, INT);
-
-	for (; exp; exp >>= 1) {
-		if (exp&01)
-			flexp = soft_mul(flexp, exp5);
-		exp5 = soft_mul(exp5, exp5);
-	}
-	if (negexp<0)
-		fl = soft_div(fl, flexp);
-	else
-		fl = soft_mul(fl, flexp);
-
-	DEXPSET(fl, DEXP(fl) + negexp*bexp);
-	p = block(FCON, NIL, NIL, DOUBLE, 0, 0); /* XXX type */
-	p->n_dcon = fl;
-	return p;
-}
-#else
-
-/*
- * Use parametric floating-point representation, as used in the package gdtoa
- * published by David M. Gay and generally available as gdtoa.tgz at
- * http://www.netlib.org/fp/ ; see also strtodg.c introduction.
- *
- * Arithmetic characteristics are described in struct FPI (explained below);
- * the actual numbers are represented (stored) in struct SF.
- * Floating-point numbers have fpi->nbits bits.
- * These numbers are regarded as integers multiplied by 2^e
- * (i.e., 2 to the power of the exponent e), where e is stored in
- * *exp by strtodg.  The minimum and maximum exponent values fpi->emin
- * and fpi->emax for normalized floating-point numbers reflect this
- * arrangement.  For example, the IEEE 754 standard for binary arithmetic
- * specifies doubles (also known as binary64) as having 53 bits, with
- * normalized values of the form 1.xxxxx... times 2^(b-1023), with 52 bits
- * (the x's) and the biased exponent b represented explicitly;
- * b is an unsigned integer in the range 1 <= b <= 2046 for normalized
- * finite doubles, b = 0 for denormals, and b = 2047 for Infinities and NaNs.
- * To turn an IEEE double into the representation used here, we multiply
- * 1.xxxx... by 2^52 (to make it an integer) and reduce the exponent
- * e = (b-1023) by 52:
- *	fpi->emin = 1 - 1023 - 52
- *	fpi->emax = 1046 - 1023 - 52
- * For fpi_binary64 initialization, we actually write -53+1 rather than -52,
- * to emphasize that there are 53 bits including one implicit bit at the
- * left of the binary point.
- * Field fpi->rounding indicates the desired rounding direction, with
- * possible values
- *	FPI_Round_zero = toward 0,
- *	FPI_Round_near = unbiased rounding -- the IEEE default,
- *	FPI_Round_up = toward +Infinity, and
- *	FPI_Round_down = toward -Infinity
- *	FPI_Round_near_from0 = to nearest, ties always away from 0
- * given in pass1.h.
- *
- * Field fpi->sudden_underflow indicates whether computations should return
- * denormals or flush them to zero.  Normal floating-point numbers have
- * bit fpi->nbits in the significand on.  Denormals have it off, with
- * exponent = fpi->emin.
- *
- * Fields fpi->explicit_one, fpi->storage, and fpi->exp_bias are only
- * relevant when the numbers are finally packed into interchange format.
- * If bit 1 is the lowest in the significand, bit fpi->storage has the sign.
- *
- * Some architectures do not use IEEE arithmetic but can nevertheless use
- * the same parametrization. They should provide their own FPI objects.
- * Fields fpi->has_inf_nan and fpi->has_neg_zero cover the non-IEEE cases
- * of lacking respectively the use of infinities and NaN, and negative zero.
- * Field fpi->has_radix_16 is for architectures (IBM, DG Nova) with base 16.
- *
- * In this implementation, the bits are stored in one large integer
- * (unsigned long long); this limits the number of bits to 64.
- * Because of the storage representation (but not the implementation),
- * exponents are restricted to 15 bits.
- *
- * Furthermore, this implementation assumes that:
- *	- integers are (obviously) stored as 2's complement
- *	- long long is (at least) 64 bits
- *	- CONSZ is (at least) 64 bits
- * There are possible issues if int is 16 bits, with divisions.
- *
- * As a result, this is also a slightly restricted software implementation of
- * IEEE 754:1985 standard. Missing parts, useless in a compiler, are:
- * - no soft_unpack(), to convert from external binary{32,64} formats
- * - no precision control (not required, dropped in :2008)
- * - no soft_sqrt() operation
- * - no soft_remainder() operation
- * - no soft_rint(), _ceil, or _floor rounding operations
- * - no binary-to-decimal conversion (can use D. Gay's dgtoa.c)
- * - signaling NaNs (SF_NoNumber; should cause SFEXCP_Invalid on every op)
- * - XXX fenv-support is pending
- * - no soft_scalb(), _logb, or _nextafter optional functions
- * It should be easy to expand it into a complete implementation.
- *
- * The operations are rounded according to the indicated type.
- * If because of FLT_EVAL_METHOD, the precision has to be greater,
- * this should be handled by the calling code.
- */
-
-/*
- * API restrictions:
- *	- type information should be between FLOAT and LDOUBLE
- */
-
 #ifndef Long
 #define Long int
 #endif
@@ -715,6 +309,29 @@ ieeefp_64_toolarge(int exp, uint64_t mant)
 	return 0;
 }
 
+static int
+ieee64_classify(SFP sfp)
+{
+	int e = sfp->fp[1] & 0x7ff00000;
+
+	if (IEEEFP_64_ISINF(sfp))
+		return SOFT_INFINITE;
+	if (IEEEFP_64_ISNAN(sfp))
+		return SOFT_NAN;
+	if (IEEEFP_64_ISZERO(sfp))
+		return SOFT_ZERO;
+	if (e)
+		return SOFT_NORMAL;
+	return SOFT_SUBNORMAL;
+}
+
+static int
+ieee64_unmake(SFP sfp, int *sign, int *exp, MINT *m)
+{
+	cerror("unmake 64");
+	return 0;
+}
+
 static void
 ieee64_make(SFP sfp, int typ, int sign, int exp, MINT *m)
 {
@@ -762,6 +379,8 @@ FPI fpi_binary64 = {
 	.expadj = 1,
 
 	.make = ieee64_make,
+	.unmake = ieee64_unmake,
+	.classify = ieee64_classify,
 };      
 #else
 #error need double definition
@@ -884,6 +503,7 @@ FPI fpi_binaryx80 = {
 
 	.make = ieeex80_make,
 	.unmake = ieeex80_unmake,
+	.classify = ieeex80_classify,
 };      
 #endif
 
@@ -1608,7 +1228,7 @@ soft_div(SFP x1p, SFP x2p, TWORD t)
 int
 soft_isz(SFP sfp)
 {
-	int r = LDOUBLE_ISZERO(sfp);
+	int r = LDBLPTR->classify(sfp) == SOFT_ZERO;
 #ifdef DEBUGFP
 	if ((sfp->debugfp == 0.0 && r == 0) || (sfp->debugfp != 0.0 && r == 1))
 		fpwarn("soft_isz", sfp->debugfp, (long double)r);
@@ -1753,8 +1373,6 @@ soft_cmp(SFP v1p, SFP v2p, int v)
 	}
 	return rv;
 }
-
-#endif /* FDFLOAT */
 
 static void
 mshr(MINT *a, int nbits, int sticky)
@@ -2073,7 +1691,9 @@ strtosf(SFP sfp, char *str, TWORD tw)
 void
 soft_huge_val(SFP sfp)
 {
-	LDOUBLE_INF(sfp, 0);
+	MINT a;
+
+	LDBLPTR->make(sfp, SOFT_INFINITE, 0, 0, &a);
 
 #ifdef DEBUGFP
 	if (sfp->debugfp != __builtin_huge_vall())
@@ -2084,7 +1704,9 @@ soft_huge_val(SFP sfp)
 void
 soft_nan(SFP sfp, char *c)
 {
-	LDOUBLE_NAN(sfp, 0);
+	MINT a;
+
+	LDBLPTR->make(sfp, SOFT_NAN, 0, 0, &a);
 }
 
 /*
